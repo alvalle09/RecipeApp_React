33434c96708bdc13eaca183f4bbba86085fe48d7
alvalle09 <phredvalle@gmail.com>
2018-08-29 23:56:23 -0500

    refactor app.js, compile issues

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,42 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.selectNewRecipe = this.selectNewRecipe.bind(this);
+    this.state = {
+      recipes: [
+        {
+          title: 'Bagel',
+          ingredients: [
+          '1 Bagel',
+          'Cream cheese'
+          ],
+          steps: [
+          'slice nagel in half',
+          'Spread on cream cheese',
+          'Enjoy!'
+          ],
+          id: 'bagel'        
+      },
+      {
+        title: 'Pizza',
+        ingredients: [
+          '1 Pizza Crust',
+          '1 Jar of Pizza Sauce',
+          '3 oz Part-Skim Mozerella Cheese'
+        ],
+        steps: [
+          'Put sacue on crust',
+          'Sprinkle a little love on it',
+          'Bake at the moon!'
+        ],
+        id: 'pizza'
+      }
+      ]
+    };
+  }
   
   render() {
     return (
