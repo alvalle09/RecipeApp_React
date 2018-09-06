447e08bed3f224ccf821864437b3ffc715dce330
alvalle09 <phredvalle@gmail.com>
2018-09-05 21:45:26 -0500

    adds navigation component, still debuggin recipe list

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-
 import Recipe from './Recipe';
+import Navigation from './Navigation';
 
 class App extends Component {
   constructor() {
@@ -9,47 +9,39 @@ class App extends Component {
       recipes: [
         {
           title: 'Bagel',  ingredients: ['1 Bagel', 'Cream cheese' ],
-          steps: ['Slice bagel in half', 'Spread on cream cheese', 'Enjoy!'
-          ],
+          steps: ['Slice bagel in half', 'Spread on cream cheese', 'Enjoy!'],
           id: 'bagel'
         },
         {
-          title: 'Pizza',
-          ingredients: [
-            '1 Pizza Crust',
-            '1 Jar of Pizza Sauce',
-            '3 oz Part-Skim Mozerella Cheese'
-          ],
-          steps: [
-            'Put sauce on crust',
-            'Sprinkle mozarella cheese over sauce',
-            'Bake at 350 degrees for 20 minutes'
-          ],
+          title: 'Pizza', ingredients: ['1 Pizza Crust', '1 Jar of Pizza Sauce', '3 oz Part-Skim Mozerella Cheese'],
+          steps: ['Put sauce on crust', 'Sprinkle mozarella cheese over sauce', 'Bake at 350 degrees for 20 minutes' ],
           id: 'pizza'
         },
       ]
-    };
+    }
   }
   
   render() {
     return (
       <div className="App">
-        <h1>React Recipe Book</h1>
+        <Navigation recipes={this.state.recipes} />
+
+        <h1>React Recipe Book</h1>    
+           
+        { 
         
-        {
           this.state.recipes.map((recipe, i) => {
             <Recipe 
-              title={recipe.title} 
-              ingredients={recipe.ingredients}
-              steps={recipe.steps}
-              key={i}
-              id={recipe.id}            
-            />
-            
+            title={recipe.title}
+            ingredients={recipe.ingredients} 
+            steps={recipe.steps}
+            key={i}
+            id={recipe.id}
+          />          
           }
         )
-      }
-      </div>
+      }        
+    </div>
     );
   }
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
-
 import PropTypes from 'prop-types';
 
 class Navigation extends Component {
+  constructor() {
+    super();
+    this.changeRecipe = this.changeRecipe.bind(this);    
+  }
+
+  changeRecipe() {
+    const buttonI = e.target.id;
+    const recipeId = buttonId.split('_')[0];
+    this.props.recipeToSelect(recipeId);
+  }
+
+
   render() {
     return (
-      <div className="Navigation">
-      </div>
+     <nav>
+       <ul>
+         {
+           this.props.recipes.map((recipe) => 
+           <li>
+             <button
+              id={recipe.id + '_button'}
+              key={recipe.id} >
+              {recipe.title}
+            </button>
+           </li>
+           )
+         }
+       </ul>
+     </nav>
     );
   }
 }
 
 Navigation.propTypes = {
-  recipes: PropTypes.array.isRequired,
+  recipes: PropTypes.array.isRequired
 };
 
 export default Navigation;
+
